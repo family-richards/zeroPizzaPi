46553465c1a3707ce8335f0936568bf843b65f07
kendell <kendell.r@outlook.com>
2019-06-24 07:03:29 -0700

    Update __init__.py

--- gameCam/gameCam/__init__.py
+++ gameCam/gameCam/__init__.py
@@ -1,1 +1,15 @@
-
+from pygame.image import save
+import pygame.camera as gamecam
+gamecam.init()
+class gameCam:
+  def __init__(self, location, width, height):
+    self.cammy = gamecam.Camera(location, (width, height))
+    self.cammy.start()
+  def start(self):
+    self.cammy.start()
+  def stop(self):
+    self.cammy.stop()
+  def takePicture(self):
+    return self.cammy.get_image()
+  def savePicture(self, filename):
+    save(self.cammy.get_image(), filename)
