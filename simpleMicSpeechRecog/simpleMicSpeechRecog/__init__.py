5fc3b711dea3c12f92977c58752212329aae25c9
kendell <kendell.r@outlook.com>
2019-06-24 07:32:54 -0700

    Update __init__.py

--- simpleMicSpeechRecog/simpleMicSpeechRecog/__init__.py
+++ simpleMicSpeechRecog/simpleMicSpeechRecog/__init__.py
@@ -1,3 +1,20 @@
 from speech_recognition import Microphone as mic
 from speech_recognition import Recognizer as recog
 class MicSpeechHearer:
+  def __init__(self):
+    self.mic = mic()
+    self.hearer = recog()
+  def adjustForNoise(self, duration=1):
+    with self.mic as micy:
+      self.hearer.adjust_for_ambient_noise(micy, duration=duration)
+  def listenAndHear(self, adjustForNoise=False, adjustDuration=1, runWhenAdjustedForNoise=null):
+    if adjustForNoise:
+      self.adjustForNoise(self, duration=adjustDuration)
+      if runWhenAdjustedForNoise != null:
+        runWhenAdjustedForNoise()
+    with self.mic as micy:
+      audio = self.hearer.listen(micy)
+      try:
+        return self.hearer.recognize_google(audio)
+      except Exception as e:
+        return ""
